1cf0b083c3fe5955cf875281f8756c7b8997fc1b
塵子曦 <britton@126.com>
2025-04-14 18:08:59 +0800

    feat 完善默认异步线程池的配置

--- leaf-base-framework/src/main/java/cn/maple/core/framework/config/GXAsyncConfig.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/config/GXAsyncConfig.java
@@ -1,7 +1,5 @@
 package cn.maple.core.framework.config;
 
-import cn.hutool.core.collection.CollUtil;
-import cn.hutool.core.text.CharSequenceUtil;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
 import org.springframework.context.annotation.Bean;
