7dc5497770b3a57411d17370e58923e149a0bb44
塵子曦 <britton@126.com>
2025-04-25 16:05:15 +0800

    feat 完善自定义转换异常类的定义

--- leaf-base-framework/src/main/java/cn/maple/core/framework/exception/GXConvertException.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/exception/GXConvertException.java
@@ -1,10 +1,58 @@
 package cn.maple.core.framework.exception;
 
+import cn.hutool.core.lang.Dict;
+import cn.maple.core.framework.code.GXResultStatusCode;
+
 /**
  * 框架数据转换异常
  */
-public class GXConvertException extends RuntimeException {
-    public GXConvertException(Throwable cause) {
-        super(cause);
+public class GXConvertException extends GXBusinessException {
+
+    public GXConvertException(String msg, int code, Dict data, Throwable e) {
+        super(msg, code, data, e);
+    }
+
+    public GXConvertException(GXResultStatusCode resultCode, String msg, Dict data, Throwable e) {
+        super(resultCode, msg, data, e);
+    }
+
+    public GXConvertException(GXResultStatusCode resultCode, Throwable e, Dict data) {
+        super(resultCode, e, data);
+    }
+
+    public GXConvertException(GXResultStatusCode resultCode, String msg, Dict data) {
+        super(resultCode, msg, data);
+    }
+
+    public GXConvertException(GXResultStatusCode resultCode, Throwable e) {
+        super(resultCode, e);
+    }
+
+    public GXConvertException(GXResultStatusCode resultCode, String msg) {
+        super(resultCode, msg);
+    }
+
+    public GXConvertException(GXResultStatusCode resultCode) {
+        super(resultCode);
+    }
+
+    public GXConvertException(String msg, Throwable e) {
+        super(msg, e);
+    }
+
+    public GXConvertException(String msg) {
+        super(msg);
+    }
+
+    public GXConvertException(String msg, int code) {
+        super(msg, code);
+    }
+
+    public GXConvertException(String msg, int code, Throwable e) {
+        super(msg, code, e);
+    }
+
+    public GXConvertException(String msg, int code, Dict data) {
+        super(msg, code, data);
     }
 }
