21dc0b38e21f7b16c809d644840d992fff6eb257
塵子曦 <britton@126.com>
2025-04-27 16:39:03 +0800

    add 新增is not null的条件表达式类

---
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/dto/inner/condition/GXConditionIsNotNULL.java
@@ -0,0 +1,21 @@
+package cn.maple.core.framework.dto.inner.condition;
+
+public class GXConditionIsNotNULL extends GXCondition<Object> {
+    public GXConditionIsNotNULL(String tableNameAlias, String fieldName, Object value) {
+        super(tableNameAlias, fieldName, value);
+    }
+
+    public GXConditionIsNotNULL(String tableNameAlias, String fieldName) {
+        this(tableNameAlias, fieldName, null);
+    }
+
+    @Override
+    public String getOp() {
+        return "is not";
+    }
+
+    @Override
+    public Object getFieldValue() {
+        return null;
+    }
+}
