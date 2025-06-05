d2ab9a49d108b9ce12846e761631a9765e64f9ad
塵子曦 <britton@126.com>
2025-06-05 15:43:30 +0800

    refactor(framework): 优化缓存机制，避免潜在的内存泄漏问题

--- leaf-base-framework/src/main/java/cn/maple/core/framework/convert/GXCGLibDataConvert.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/convert/GXCGLibDataConvert.java
@@ -2,6 +2,7 @@ package cn.maple.core.framework.convert;
 
 import cn.hutool.core.bean.BeanUtil;
 import cn.hutool.core.convert.Convert;
+import cn.hutool.core.map.WeakConcurrentMap;
 import cn.hutool.core.util.ClassUtil;
 import cn.hutool.core.util.ReflectUtil;
 import cn.hutool.core.util.TypeUtil;
@@ -167,7 +168,7 @@ public class GXCGLibDataConvert implements Converter {
      * 使用ConcurrentHashMap确保线程安全
      * </p>
      */
-    private static final Map<Class<?>, GXCGLibDataConvert> CONVERTER_CACHE = new ConcurrentHashMap<>(1024);
+    private static final Map<Class<?>, GXCGLibDataConvert> CONVERTER_CACHE = new WeakConcurrentMap<>(new ConcurrentHashMap<>(1024));
 
     /**
      * 字段缓存
