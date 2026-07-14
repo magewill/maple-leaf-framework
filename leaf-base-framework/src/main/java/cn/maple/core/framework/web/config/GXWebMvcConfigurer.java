ebbefa6c5e376cb30d0c9d992c4cf497d14564c0
塵子曦 <britton@126.com>
2026-07-14 10:39:06 +0800

    feat(fix): 修复spring中的Pageable不能正确获取到参数

--- leaf-base-framework/src/main/java/cn/maple/core/framework/web/config/GXWebMvcConfigurer.java
+++ leaf-base-framework/src/main/java/cn/maple/core/framework/web/config/GXWebMvcConfigurer.java
@@ -4,17 +4,15 @@ import cn.maple.core.framework.util.GXSpringContextUtils;
 import cn.maple.core.framework.web.interceptor.GXAuthorizationInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.IdentityHashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Configuration
+@EnableSpringDataWebSupport
 @Slf4j
 public class GXWebMvcConfigurer implements WebMvcConfigurer {
     @Override
